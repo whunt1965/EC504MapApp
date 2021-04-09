43b4ab96b2a40667fdeff279ce829529e8812b64
Apollo Lo <apollol@bu.edu>
2021-04-09 04:18:00 -0400

    debug dijkstra

--- src/Algos/Dijkstra.py
+++ src/Algos/Dijkstra.py
@@ -1,55 +1,82 @@
 # Dijkstra Algo here...
-import HeapQ 
 
-def Dijkstra(Nodes,Map, start, end): 	
-	Mark = [-1 for i in range(len(Nodes))] 
+from src.Objects.HeapQ import HeapQ
 
-	
+LARGE = 9999999999
 
+
+def getRoute(node, Nodes, Map):
+    ret = []
+    while not node.isSrc:
+        ret.append(node.id)
+        parent = node.Parent
+        if parent == -1:
+            print("Something went wrong!")
+            return None, None  # Something went wrong -- lets figure out a better solution
+        node = Nodes[Map.get(parent)]
+    ret.append(node.id)  # Add src node
+    ret.reverse()
+    return ret
+
+
+def Dijkstra(Nodes, Map, start, end):
+    Mark = [-1 for i in range(len(Nodes))]
     src = Nodes[Map.get(start)]
     src.key = 0
     min_v = src
     min_d = 0
-    finished = 1;
+    finished = 1
 
     while finished < len(Nodes):
 
-    	for edge in min_node.edges: #go through edges of a node with minimum key
-    		v = Map[edge.end]
-    		dv = min_v.key + edge.weight
-    		if Nodes[v].key > dv:
+        for edge in min_v.edges:  # go through edges of a node with minimum key
+            v = Map[edge.end]
+            dv = min_v.key + edge.weight
+            if Nodes[v].key > dv:
                 Nodes[v].key = dv
-                Nodes[v].Parent = node.id
+                Nodes[v].Parent = min_v.id
 
-       
         min_d = LARGE
         for node in Nodes:
-        	if Mark[node.id] < 1: #if not visited 
-        		if node.key < min_d:
-        			min_d = node.key
-        			min_v = node
-        		
-        Mark[min_node.index] = 1
-        finished++
+            if Mark[node.index] < 1:  # if not visited
+                if node.key < min_d:
+                    min_d = node.key
+                    min_v = node
+
+        Mark[min_v.index] = 1
+        finished += 1
+
+    node = Nodes[Map.get(end)]  # Get destination node
+    dist = node.key  # Get final distance
+    route = getRoute(node, Nodes, Map)  # Computer Predecessor Array (route)
+    return route, dist
 
 
-def Dijkstra_HeapQ(Nodes,Map, start, end):
-    queue = heapQ()
+def Dijkstra_HeapQ(Nodes, Map, start, end):
+    queue = HeapQ(len(Nodes))
     src = Nodes[Map.get(start)]
     src.key = 0
-
     for node in Nodes:
         queue.insert(node)
 
-
-    while(queue.isEmpty() == false):
-        min_node = queue.remove_min()
-
-        for edge in min_node.edges: #go through edges of a node with minimum key
+    count = 0
+    while queue.size > 0:
+        min_v = queue.remove_min()
+        count +=1
+        if count == 10:
+            print(queue.size)
+            count = 0
+        for edge in min_v.edges:  # go through edges of a node with minimum key
             v = Map[edge.end]
             dv = min_v.key + edge.weight
             if Nodes[v].key > dv:
                 Nodes[v].key = dv
                 Nodes[v].Parent = node.id
-                queue.decreaseKey(Nodes[v].position, N[v].key)
-            
+                queue.decreaseKey(Nodes[v].index, Nodes[v].key)
+        print("size" + str(queue.size))
+
+    print("gather")
+    node = Nodes[Map.get(end)]  # Get destination node
+    dist = node.key  # Get final distance
+    route = getRoute(node, Nodes, Map)  # Computer Predecessor Array (route)
+    return route, dist
--- src/Objects/HeapQ.py
+++ src/Objects/HeapQ.py
@@ -4,61 +4,71 @@ class HeapQ:
     def __init__(self, maxsize):
         self.maxsize = maxsize
         self.size = 0
-        self.arr = [None]*maxsize
+        self.arr = [None] * maxsize
 
     def insert(self, node):
-    	if self.size >= maxsize:
-    		return
-    	arr[size] = node
-		size++
-		self.__upHeap(size-1)
+        if self.size < self.maxsize:
+            self.arr[self.size] = node
+            self.size += 1
+            self.__upHeap(self.size - 1)
 
-	def remove_min(self):
-		if self.size == 0:
-			return
-		size--
-		temp = arr[0]
-		if (size != 0):
-			arr[0] = arr[size]
-			arr[0].position = 0;
-			self.__downHeap(0)
-		temp.position = -1 #out of heap
-		return temp
-
-	def decreaseKey(self, position, value):
-		self.arr[position].key = value
-		self.__upHeap(position)
+    def remove_min(self):
+        if self.size == 0:
+            print("size is 0")
+        else:
+            self.size -= 1
+            temp = self.arr[0]
+            if self.size != 0:
+                self.arr[0] = self.arr[self.size]
+                self.arr[0].index = 0
+                self.__downHeap(0)
+            temp.index = -1  # out of heap
+            return temp
 
 
-	def __upHeap(self, new_pos):
-		temp_node = self.arr[new_pos]
-		int parent = (new_pos-1)/2;
-		while ((new_pos != 0) && temp_node.key < self.arr[parent].key):
-			self.arr[new_pos] = self.arr[parent].key
-			self.arr[new_pos].position = new_pos
-			new_pos = parent
-			parent = (new_pos-1)/2
-		self.arr[new_pos] = temp_node
-		self.arr[new_pos].position = new_pos
+    def decreaseKey(self, index, value):
+        self.arr[index].key = value
+        #print("decrease key time")
+        self.__upHeap(index)
 
-	def __downHeap(self, root):
-		temp = arr[root]
-		if (size == 1):
-			return
-		int child = 2*root + 1
-		while true:
-			if (child < size -1):
-				if (self.arr[child+1].key < self.arr[child].key): #pick smaller child
-					child++
-			if (self.arr[child].key < temp.key):
-				self.arr[root] = self.arr[child]
-				self.arr[root].position = root
-				root = child
-				child = 2*root+1
-			if (child >= size):
-				break
-		self.arr[root] = temp
-		self.arr[root].position = root
+    def Empty(self):
 
-        
+        return self.size <= 0
 
+    def Full(self):
+        return self.size >= self.maxsize
+
+    def __upHeap(self, new_pos):
+        temp_node = self.arr[new_pos]
+        parent = int((new_pos - 1) / 2)
+        #print("inside decrease")
+        while (new_pos != 0) and temp_node.key < self.arr[parent].key:
+            self.arr[new_pos] = self.arr[parent]
+            self.arr[new_pos].index = new_pos
+            new_pos = parent
+            parent = int((new_pos - 1) / 2)
+            #print("parent")
+        self.arr[new_pos] = temp_node
+        self.arr[new_pos].index = new_pos
+
+    def __downHeap(self, root):
+        temp = self.arr[root]
+        if self.size == 1:
+            print("this is root, no children")
+        else:
+            child = 2 * root + 1
+            while True:
+                if child < self.size - 1:
+                    if self.arr[child + 1].key < self.arr[child].key:  # pick smaller child
+                        child += 1
+                if self.arr[child].key < temp.key:
+                    self.arr[root] = self.arr[child]
+                    self.arr[root].index = root
+                    root = child
+                    child = 2 * root + 1
+                else:
+                    break
+                if child >= self.size:
+                    break
+            self.arr[root] = temp
+            self.arr[root].index = root
