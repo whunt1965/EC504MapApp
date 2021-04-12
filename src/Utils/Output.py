daf1219185525d74372e2d1f37292584386d2077
Ken Krebs <56164075+ken-krebs@users.noreply.github.com>
2021-04-12 15:04:00 -0400

    Added a main function to take inputs

--- src/Utils/Output.py
+++ src/Utils/Output.py
@@ -4,7 +4,7 @@ import os
 # Create new HTML file, insert link to saved plot, 
 # add other relevant information
 
-def giveOutput():
+def giveOutput(src, dst, route, city):
     f = open('output.html','w')
     message = """<html>
     <head></head>
@@ -12,26 +12,34 @@ def giveOutput():
     """
     f.write(message)
 
-    src = "42.3505, -71.1054"
-    dst = "42.3467, -71.0972"
+  
     header = "<h2>"
     endheader = "<h2/>"
-    locationInfo = header + src + endheader + header + dst + endheader
+    locationInfo = header + str(src) + endheader + header + str(dst) + endheader
     
     f.write(locationInfo)
     
     image="""
-    <img src="pic.png" />
+    <img src="pic.png""/>
     </body>
     </html>"""
-    f.write(image)
-    i=0
-    p = "<p>"
-    endP = "<p/>"
-    while(i < 10):
-        direction = p + "direction number: " + str(i) + endP
-        f.write(direction)
-        i = i+1
+    image2="""
+    <img src="map2.jpeg" />
+    </body>
+    </html>"""
+    if city == 4:
+        f.write(image2)
+        f.write("<h1>SWEAR TO ME!!!!!</h1>")
+
+    else:
+        f.write(image)
+        i=0
+        p = "<p>"
+        endP = "<p/>"
+        while(i < 10):
+            direction = p + "direction number: " + str(i) + endP
+            f.write(direction)
+            i = i+1
     f.close()
     os.system("open output.html")
 
